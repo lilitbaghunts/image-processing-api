713c2d7b111312c57b812cb0c152bb2c8934b4b8
Lilit Baghunts <6624092+lilitbaghunts@users.noreply.github.com>
2024-07-17 00:15:55 +0400

    Update productController.js

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -13,17 +13,20 @@ const createProductImage = async (req, res, next) => {
       return res.status(400).json('product not found')
     }
     let store = await Store.findByPk(product.get('store_id'))
-    runWorker({
+    if (!store) {
+      return res.status(400).json('store not found');
+    }
+    const workerResult = await runWorker({
       productId: product.get('id'),
       productImage: req.file.filename,
       watermarkImage: store.watermark_image
-    }).then(() => {
-      const image = product.get('image')
-      if (image && fs.existsSync(`${imagePath}/${image}`)) {
-        fs.unlinkSync(`${imagePath}/${image}`)
-      }
-      product.update({ image: req.file.filename })
     })
+    
+    const image = product.get('image')
+    if (image && fs.existsSync(`${imagePath}/${image}`)) {
+      fs.unlinkSync(`${imagePath}/${image}`)
+    }
+    await product.update({ image: req.file.filename })
 
     return res.status(200).json('success')
   } catch(err) {
@@ -36,14 +39,13 @@ const getProductImage = async (req, res, next) => {
   try {
     const { id } = req.params
     let product = await Product.findByPk(id, { raw: true })
-    if (fs.existsSync(`${imagePath}/${product.image}`)) {
-      fs.createReadStream(`${imagePath}/${product.image}`)
-          .pipe(res);
+    if (product && fs.existsSync(`${imagePath}/${product.image}`)) {
+      fs.createReadStream(`${imagePath}/${product.image}`).pipe(res);
     }
     return res.status(404).json({ message: 'no image found' })
   } catch(err) {
     console.log(err)
-    next()
+    next(err)
   }
 
 }
@@ -52,14 +54,13 @@ const getProductThumbnail = async (req, res, next) => {
   try {
     const { id } = req.params
     let product = await Product.findByPk(id, { raw: true })
-    if (fs.existsSync(`./uploads/thumbnails/thumbnail-product-${product.id}.jpg`)) {
-      return fs.createReadStream(`./uploads/thumbnails/thumbnail-product-${product.id}.jpg`)
-          .pipe(res);
+    if (product && fs.existsSync(`./uploads/thumbnails/thumbnail-product-${product.id}.jpg`)) {
+      return fs.createReadStream(`./uploads/thumbnails/thumbnail-product-${product.id}.jpg`).pipe(res);
     }
     return res.status(404).json({ message: 'no image found' })
   } catch(err) {
     console.log(err)
-    next()
+    next(err)
   }
 }
 
